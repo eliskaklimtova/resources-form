8db2bdd5843a9b77fdcbecb93ea5e784b294f4a1
Eliska Koci <eli.klimtova@gmail.com>
2023-07-25 18:08:04 +0200

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [resources, setResources] = useState([]);
+
+  const [form, setForm] = useState({
+    title: "",
+    link: "",
+    type: "",
+    notes: "",
+    author: "",
+    topic: "",
+    tags: "",
+    date: "",
+    event: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleClick = () => {
+    setResources([...resources, { ...form }]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="min-h-screen flex flex-col items-center m-auto p-20 prose">
+      <h1>SoftCraft Resources</h1>
+      <div className="form-control min-w-full">
+        <label htmlFor="title">Title</label>
+        <input
+          value={form.title}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="title"
+          name="title"
+          size="10"
+          required={true}
         />
+        <label htmlFor="link">Link</label>
+        <input
+          value={form.link}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="link"
+          name="link"
+          size="10"
+          required={true}
+        />
+        <label htmlFor="type">Type</label>
+        <input
+          value={form.type}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="type"
+          name="type"
+          size="10"
+          required={true}
+        />
+        <label htmlFor="notes">Notes</label>
+        <input
+          value={form.notes}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="notes"
+          name="notes"
+          size="10"
+        />
+        <label htmlFor="author">Author</label>
+        <input
+          value={form.author}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="author"
+          name="author"
+          size="10"
+        />
+        <label htmlFor="topic">Topic</label>
+        <input
+          value={form.topic}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="topic"
+          name="topic"
+          size="10"
+        />
+        <label htmlFor="tags">Tags</label>
+        <input
+          value={form.tags}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="tags"
+          name="tags"
+          size="10"
+        />
+        <label htmlFor="date">Date</label>
+        <input
+          value={form.date}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="date"
+          name="date"
+          size="10"
+        />
+        <label htmlFor="event">Event</label>
+        <input
+          value={form.event}
+          onChange={handleChange}
+          className="input input-bordered"
+          type="text"
+          id="event"
+          name="event"
+          size="10"
+        />
+        <button className="btn btn-primary mt-5" onClick={handleClick}>
+          Add resource
+        </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {resources.length !== 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Link</th>
+              <th>Type</th>
+              <th>Notes</th>
+              <th>Author</th>
+              <th>Topic</th>
+              <th>Tags</th>
+              <th>Date</th>
+              <th>Event</th>
+            </tr>
+          </thead>
+          <tbody>
+            {resources.map((resource) => (
+              <tr key={resource.link}>
+                <td>{resource.title}</td>
+                <td>
+                  <a href={resource.link} target="_blank">
+                    {resource.link}
+                  </a>
+                </td>
+                <td>{resource.type}</td>
+                <td>{resource.notes}</td>
+                <td>{resource.author}</td>
+                <td>{resource.topic}</td>
+                <td>{resource.tags}</td>
+                <td>{resource.date}</td>
+                <td>{resource.event}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </main>
-  )
+  );
 }
